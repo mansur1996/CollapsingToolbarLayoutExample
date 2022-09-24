f9dcbb9a4b752eb2850b9f6817e50dac68a450ae
Mr Mansur <mirzayevmn96@gmail.com>
2022-09-24 08:05:52 -0700

    first commit

---
+++ app/src/main/java/com/example/collepsingtoolbar/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.collepsingtoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
